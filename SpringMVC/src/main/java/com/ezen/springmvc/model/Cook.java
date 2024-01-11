01e076722bd5a6fd49522b3a5750500a58ce18c7
DESKTOP-1NV2HPB\Gi7E-36 <tony577@naver.com>
2024-01-11 12:51:57 +0900

    24/01/11 am

---
+++ SpringMVC/src/main/java/com/ezen/springmvc/model/Cook.java
@@ -0,0 +1,22 @@
+package com.ezen.springmvc.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Cook {
+	@Autowired
+	Dish dish;
+	@Autowired
+	Food food;
+
+	private Cook() {}
+	
+	@Override
+	public String toString() {
+		return String.format("%s/%s", food, dish);
+	}
+	
+
+}
